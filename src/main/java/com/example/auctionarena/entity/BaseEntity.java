10883c8a15b43c514c8935683e1a1ac38c56ed74
2naaayoon <inayoon78@gmail.com>
2024-05-14 19:53:06 +0900

    pageDto, BaseEntity 추가

---
+++ src/main/java/com/example/auctionarena/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.example.auctionarena.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@EntityListeners(value = AuditingEntityListener.class)
+@MappedSuperclass // BaseEntity 상속할 경우 필드를 컬럼으로 인식하기
+public abstract class BaseEntity {
+
+    // update 불가능
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+}
