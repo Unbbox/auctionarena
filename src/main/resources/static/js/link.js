53d838ea25c53bae3d66e19530fca7d47a6265d9
namjunhee <njh0581@naver.com>
2024-05-14 19:20:09 +0900

    카테고리 css

--- src/main/resources/static/js/link.js
+++ src/main/resources/static/js/link.js
@@ -1,4 +1,24 @@
-document.querySelector(".link").addEventListener("click", () => {
-  window.location.href = "anime-details.html";
+document.querySelector(".link").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const heartdiv = e.target.querySelector(".wish");
+
+  heartdiv.addEventListener("click", (e) => {
+    // const heart = heartbtn.querySelector(".fa-heart");
+    console.log(heartbtn);
+    const heartbtn = heartdiv.querySelector("i");
+
+    if (heartbtn.classList.contains("fa-regular")) {
+      heartbtn.classList.remove("fa-regular");
+      heartbtn.classList.add("fa-solid");
+    } else if (heartbtn.classList.contains("fa-solid")) {
+      heartbtn.classList.remove("fa-solid");
+      heartbtn.classList.add("fa-regular");
+    }
+  });
   // window.location.href = "https://www.naver.com";
+  // heartbtn = e.target;
+  // console.log(heartbtn);
+  // console.log(heartbtn.querySelector(".fa-heart"));
+  window.location.href = "anime-details";
 });
