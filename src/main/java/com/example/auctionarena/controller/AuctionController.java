a1592dda6bb6545a5a28142cbb550dfcad8ca51e
namjunhee <njh0581@naver.com>
2024-05-16 15:40:11 +0900

    카테고리 분류

--- src/main/java/com/example/auctionarena/controller/AuctionController.java
+++ src/main/java/com/example/auctionarena/controller/AuctionController.java
@@ -1,33 +1,59 @@
 package com.example.auctionarena.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("auctionArena")
 @Log4j2
 @Controller
 public class AuctionController {
 
-    // 전체 상품
-    @GetMapping("/categories")
-    public String getCategory() {
-        log.info("전체 상품 목록 페이지 요청");
+  // 전체 상품
+  @GetMapping("/fashion-category")
+  public String getFashionCategory() {
+    log.info("전체 상품 목록 페이지 요청");
 
-        return "auctionArena/categories";
-    }
+    return "auctionArena/fashion-category";
+  }
 
-    // 판매 등록
-    @GetMapping("/product_details")
-    public void getDetails() {
-        log.info("물품 판매 페이지 요청");
-    }
+  @GetMapping("/mobile-category")
+  public String getMobileCategory() {
+    log.info("전체 상품 목록 페이지 요청");
 
-    // @GetMapping("/categories")
-    // public void getCategory() {
-    // //
-    // }
+    return "auctionArena/fashion-category";
+  }
+
+  @GetMapping("/electronic-category")
+  public String getElectronicCategory() {
+    log.info("전체 상품 목록 페이지 요청");
+
+    return "auctionArena/fashion-category";
+  }
+
+  @GetMapping("/game-category")
+  public String getGameCategory() {
+    log.info("전체 상품 목록 페이지 요청");
+
+    return "auctionArena/game-category";
+  }
+
+  @GetMapping("/trib-category")
+  public String getTribCategory() {
+    log.info("전체 상품 목록 페이지 요청");
+
+    return "auctionArena/trib-category";
+  }
+
+  // 판매 등록
+  @GetMapping("/product_details")
+  public void getDetails() {
+    log.info("물품 판매 페이지 요청");
+  }
+  // @GetMapping("/categories")
+  // public void getCategory() {
+  // //
+  // }
 
 }
