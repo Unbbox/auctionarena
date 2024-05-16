bc9a8c697a41637c565ef60e9f17df0243df7087
namjunhee <njh0581@naver.com>
2024-05-16 16:52:02 +0900

    카테고리 repository

--- src/test/java/com/example/auctionarena/repository/ProductRepositoryTest.java
+++ src/test/java/com/example/auctionarena/repository/ProductRepositoryTest.java
@@ -1,34 +1,34 @@
 package com.example.auctionarena.repository;
 
+import com.example.auctionarena.entity.Category;
+import com.example.auctionarena.entity.Product;
 import java.util.Date;
 import java.util.stream.IntStream;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.example.auctionarena.entity.Category;
-import com.example.auctionarena.entity.Product;
-
 @SpringBootTest
 public class ProductRepositoryTest {
 
-    @Autowired
-    private ProductRepository productRepository;
+  @Autowired
+  private ProductRepository productRepository;
 
-    @Test
-    public void productInsertTest() {
-        // 제품 데이터 추가
-        IntStream.rangeClosed(1, 50).forEach(i -> {
-            Product product = Product.builder()
-                    .title("제품" + i)
-                    .content("개쩌는 상품..!" + i)
-                    .startPrice(i * 1000L)
-                    .biddingDate(Integer.toString(i % 7))
-                    .category(Category.builder().cno(i % 5L).build())
-                    .build();
-            productRepository.save(product);
-
-        });
-    }
+  @Test
+  public void productInsertTest() {
+    // 제품 데이터 추가
+    IntStream
+      .rangeClosed(1, 50)
+      .forEach(i -> {
+        Product product = Product
+          .builder()
+          .title("제품" + i)
+          .content("개쩌는 상품..!" + i)
+          .startPrice(i * 1000L)
+          .biddingDate(Integer.toString(i % 7))
+          .category(Category.builder().cno(i % 5L).build())
+          .build();
+        productRepository.save(product);
+      });
+  }
 }
