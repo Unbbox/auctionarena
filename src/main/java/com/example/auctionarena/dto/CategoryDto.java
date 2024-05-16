bf81a556d11c2361b5a9b4110c76e5adc4d812e6
namjunhee <njh0581@naver.com>
2024-05-16 16:30:19 +0900

    카테고리 dto

---
+++ src/main/java/com/example/auctionarena/dto/CategoryDto.java
@@ -0,0 +1,19 @@
+package com.example.auctionarena.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class CategoryDto {
+
+  private Long cno;
+  private String categoryName;
+}
