14cf4f30f3b1f29647e52328bfb738dc21b82bdd
namjunhee <njh0581@naver.com>
2024-05-17 18:04:23 +0900

    prductreposotoryTest

--- src/test/java/com/example/auctionarena/repository/ProductRepositoryTest.java
+++ src/test/java/com/example/auctionarena/repository/ProductRepositoryTest.java
@@ -5,11 +5,7 @@ import com.example.auctionarena.entity.Category;
 import com.example.auctionarena.entity.Member;
 import com.example.auctionarena.entity.Product;
 import com.example.auctionarena.entity.ProductImage;
-<<<<<<< HEAD
-=======
-
 import java.time.LocalDateTime;
->>>>>>> 9ba26b30717a939b69fec7188d7edaf30f5ef89b
 import java.util.Date;
 import java.util.UUID;
 import java.util.stream.IntStream;
@@ -20,80 +16,79 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class ProductRepositoryTest {
 
-    @Autowired
-    private ProductRepository productRepository;
+  @Autowired
+  private ProductRepository productRepository;
 
-    @Autowired
-    private ProductImageRepository productImageRepository;
+  @Autowired
+  private ProductImageRepository productImageRepository;
 
-    @Autowired
-    private BiddingRepository biddingRepository;
+  @Autowired
+  private BiddingRepository biddingRepository;
 
-    @Test
-    public void productInsertTest() {
+  @Test
+  public void productInsertTest() {
+    IntStream
+      .rangeClosed(1, 50)
+      .forEach(i -> {
+        Long cno = (long) (Math.random() * 6) + 1;
+        Category category = Category.builder().cno(cno).build();
 
-        IntStream.rangeClosed(1, 50).forEach(i -> {
-            Long cno = (long) (Math.random() * 6) + 1;
-            Category category = Category.builder().cno(cno).build();
+        Long mid = (long) (Math.random() * 99) + 1;
+        Member member = Member.builder().mid(mid).build();
 
-            Long mid = (long) (Math.random() * 99) + 1;
-            Member member = Member.builder().mid(mid).build();
+        Product product = Product
+          .builder()
+          .title("제품" + i)
+          .content("개쩌는 상품..!" + i)
+          .startPrice(i * 1000L)
+          .biddingDate((i % 7L) + 1)
+          .member(member)
+          .category(category)
+          .build();
+        productRepository.save(product);
 
-            Long mid = (long) (Math.random() * 99) + 1;
-            Member member = Member.builder().mid(mid).build();
+        int count = (int) (Math.random() * 10) + 1;
 
-            Product product = Product.builder()
-                    .title("제품" + i)
-                    .content("개쩌는 상품..!" + i)
-                    .startPrice(i * 1000L)
-                    .biddingDate((i % 7L) + 1)
-                    .member(member)
-                    .category(category)
-                    .build();
-            productRepository.save(product);
+        for (int j = 0; j < count; j++) {
+          ProductImage pImage = ProductImage
+            .builder()
+            .uuid(UUID.randomUUID().toString())
+            .product(product)
+            .imgName("img" + j + ".jpg")
+            .build();
+          productImageRepository.save(pImage);
+        }
+      });
+  }
 
-            int count = (int) (Math.random() * 10) + 1;
+  @Test
+  public void biddingDataInsertTest() {
+    // 응찰 관련 데이터 삽입
+    IntStream
+      .rangeClosed(1, 50)
+      .forEach(i -> {
+        Long pno = (long) (Math.random() * 50) + 2;
+        Product product = Product.builder().pno(pno).build();
 
-            for (int j = 0; j < count; j++) {
-                ProductImage pImage = ProductImage.builder()
-                        .uuid(UUID.randomUUID().toString())
-                        .product(product)
-                        .imgName("img" + j + ".jpg")
-                        .build();
-                productImageRepository.save(pImage);
-            }
-        });
-    }
+        Long mid = (long) (Math.random() * 99) + 1;
+        Member member = Member.builder().mid(mid).build();
 
-    @Test
-    public void biddingDataInsertTest() {
-        // 응찰 관련 데이터 삽입
-        IntStream.rangeClosed(1, 50).forEach(i -> {
-
-            Long pno = (long) (Math.random() * 50) + 2;
-            Product product = Product.builder().pno(pno).build();
-
-            Long mid = (long) (Math.random() * 99) + 1;
-            Member member = Member.builder().mid(mid).build();
-
-            Bidding bidding = Bidding.builder()
-                    .biddingPrice(i * 10000L)
-                    .product(product)
-                    .member(member)
-                    .build();
-            biddingRepository.save(bidding);
-        });
-    }
-
-            // for (int j = 0; j < count; j++) {
-            // ProductImage pImage = ProductImage.builder()
-            // .uuid(UUID.randomUUID().toString())
-            // .product(product)
-            // .imgName("img" + j + ".jpg")
-            // .build();
-            // productImageRepository.save(pImage);
-            // }
-        });
-    }
+        Bidding bidding = Bidding
+          .builder()
+          .biddingPrice(i * 10000L)
+          .product(product)
+          .member(member)
+          .build();
+        biddingRepository.save(bidding);
+      });
+  }
+  // for (int j = 0; j < count; j++) {
+  // ProductImage pImage = ProductImage.builder()
+  // .uuid(UUID.randomUUID().toString())
+  // .product(product)
+  // .imgName("img" + j + ".jpg")
+  // .build();
+  // productImageRepository.save(pImage);
+  // }
 
 }
