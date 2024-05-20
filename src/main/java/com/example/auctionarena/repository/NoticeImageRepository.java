e0b994c3da6fbae3b1602e5d1a7c95fec7f689fc
2naaayoon <inayoon78@gmail.com>
2024-05-20 17:28:16 +0900

    notice image list, read

--- src/main/java/com/example/auctionarena/repository/NoticeImageRepository.java
+++ src/main/java/com/example/auctionarena/repository/NoticeImageRepository.java
@@ -3,7 +3,8 @@ package com.example.auctionarena.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.auctionarena.entity.NoticeImage;
+import com.example.auctionarena.repository.noticeSearch.SearchNoticeRepository;
 
-public interface NoticeImageRepository extends JpaRepository<NoticeImage, Long> {
+public interface NoticeImageRepository extends JpaRepository<NoticeImage, Long>, SearchNoticeRepository {
 
 }
--- src/main/java/com/example/auctionarena/repository/NoticeRepository.java
+++ src/main/java/com/example/auctionarena/repository/NoticeRepository.java
@@ -1,13 +1,9 @@
 package com.example.auctionarena.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.example.auctionarena.entity.Notice;
-import com.example.auctionarena.repository.noticeSearch.SearchNoticeRepository;
 
-public interface NoticeRepository extends JpaRepository<Notice, Long>, SearchNoticeRepository {
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
 }
