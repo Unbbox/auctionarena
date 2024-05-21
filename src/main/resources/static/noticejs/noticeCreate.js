c3e971b09d91471c7b86cabe6d78116e1f349ca0
2naaayoon <inayoon78@gmail.com>
2024-05-21 19:24:44 +0900

    이미지 업로드 추가

--- src/main/resources/static/noticejs/noticeCreate.js
+++ src/main/resources/static/noticejs/noticeCreate.js
@@ -1,14 +1,27 @@
-const fileInput = document.querySelector("#fileInput");
+// x를 누르면 파일 삭제 요청
+document.querySelector(".uploadResult").addEventListener("click", (e) => {
+  e.preventDefault();
 
-const uploadResult = document.querySelector(".uploadResult ul");
+  const currentLi = e.target.closest("li");
 
-function checkExtension(fileName) {
-  const regex = /(.*?).(png|gif|jpg)$/;
+  const filePath = e.target.closest("a").dataset.file;
+  console.log("filePath", filePath);
 
-  console.log(regex.test(fileName));
-  return regex.test(fileName);
-}
+  const formData = new FormData();
+  formData.append("filePath", filePath);
 
-function showUploadImages(arr) {
-  console.log("showUploadImages ", arr);
-}
+  fetch("/upload/remove", {
+    method: "post",
+    headers: {
+      "X-CSRF-TOKEN": CSSFontFeatureValuesRule,
+    },
+    body: formData,
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      console.log(data);
+      if (data) {
+        currentLi.remove();
+      }
+    });
+});
---
+++ src/main/resources/static/noticejs/noticeUpload.js
@@ -0,0 +1,80 @@
+const fileInput = document.querySelector("#fileInput");
+
+const uploadResult = document.querySelector(".uploadResult ul");
+
+function checkExtension(fileName) {
+  const regex = /(.*?).(png|gif|jpg)$/;
+
+  console.log(regex.test(fileName));
+  return regex.test(fileName);
+}
+
+// 업로드이미지 보여주기
+function showUploadImages(arr) {
+  console.log("showUploadImages ", arr);
+
+  let tags = "";
+
+  arr.forEach((obj, idx) => {
+    tags += `<li data-name="${obj.fileName}" data-path="${obj.folderPath}" data-uuid="${obj.uuid}">`;
+    tags += `<div>`;
+    tags += `<a href=""><img src="/upload/display?fileName=${obj.imageURL}" class="block"></a>`;
+    tags += `<span class="text-sm d-inline-block mx-1">${obj.fileName}</span>`;
+    tags += `<a href="#" data-file="${obj.imageURL}">`;
+    tags += `<i class="fa-solid fa-xmark"></i>`;
+    tags += `</a></div></li>`;
+  });
+  uploadResult.insertAdjacentHTML("beforeend", tags);
+}
+
+// fileInput change 이벤트
+fileInput.addEventListener("change", (e) => {
+  const files = e.target.files;
+
+  const formData = new FormData();
+  for (let index = 0; index < files.length; index++) {
+    if (checkExtension(files[index].name)) {
+      formData.append("uploadFiles", files[index]);
+    }
+  }
+
+  for (const value of formData.values()) {
+    console.log(value);
+  }
+
+  fetch("/upload/uploadAjax", {
+    method: "post",
+    // headers: {
+    //   "X-CSRF-TOKEN": CSSFontFeatureValuesRule,
+    // },
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      showUploadImages(data);
+    });
+});
+
+// register, modify 중복 사용
+document.querySelector("#create-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const form = e.target;
+
+  const attachInfos = document.querySelectorAll(".uploadResult ul li");
+  console.log(attachInfos);
+
+  let result = "";
+  attachInfos.forEach((obj, idx) => {
+    result += `<input type='hidden' value='${obj.dataset.path}' name='noticeImageDtos[${idx}].npath'>`;
+    result += `<input type='hidden' value='${obj.dataset.uuid}' name='noticeImageDtos[${idx}].nuuid'>`;
+    result += `<input type='hidden' value='${obj.dataset.name}' name='noticeImageDtos[${idx}].nimgName'>`;
+  });
+  form.insertAdjacentHTML("beforeend", result);
+
+  console.log(form.innerHTML);
+
+  form.submit();
+});
