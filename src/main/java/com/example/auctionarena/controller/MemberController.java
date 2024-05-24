1d2d0c3a9918051a5a9496347832b221a0e2e047
2naaayoon <inayoon78@gmail.com>
2024-05-24 17:31:54 +0900

    회원가입 추가

--- src/main/java/com/example/auctionarena/controller/MemberController.java
+++ src/main/java/com/example/auctionarena/controller/MemberController.java
@@ -1,13 +1,22 @@
 package com.example.auctionarena.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.auctionarena.dto.MemberDto;
 import com.example.auctionarena.service.MemberService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RequestMapping("member")
 @RequiredArgsConstructor
@@ -23,9 +32,28 @@ public class MemberController {
     }
 
     @GetMapping("/signup")
-    public String signup() {
+    public void signup(MemberDto memberDto) {
         log.info("회원가입 폼 요청");
-        return "member/signup";
+    }
+
+    @PostMapping("/signup")
+    public String postSignup(@Valid MemberDto dto, BindingResult result, RedirectAttributes rttr) {
+        log.info("회원가입 요청 {}", dto);
+
+        if (result.hasErrors()) {
+            return "/member/signup";
+        }
+
+        String email = "";
+        try {
+            email = service.signup(dto);
+        } catch (Exception e) {
+            rttr.addFlashAttribute("error", e.getMessage());
+            return "redirect:/member/signup";
+        }
+
+        rttr.addFlashAttribute("email", email);
+        return "redirect:/member/login";
     }
 
     @GetMapping("/edit-password")
