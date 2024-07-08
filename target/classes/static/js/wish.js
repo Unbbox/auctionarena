3c0d1f9fb39ca517181c4aca140d4cd4d577bfd7
unboxx <ehe110894@naver.com>
2024-07-09 01:33:22 +0900

    wish js 수정

--- target/classes/static/js/wish.js
+++ target/classes/static/js/wish.js
@@ -3,12 +3,47 @@ const wishBtn = document.querySelector(".follow_btn");
 const wishForm = document.querySelector(".wishForm");
 // const mid = wishForm.querySelector("#mid");
 
+const showWish = () => {
+  const btn = wishBtn.querySelector(".fa-heart");
+  console.log("찜목록 wish : ", user);
+
+  // if (user != 'anonymousUser') {
+  fetch(`/wish/${pno}/${mid}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        console.log("data.mid: ", data.mid);
+        console.log("mid: ", mid);
+        console.log("type of mid, data.mid : ", typeof mid, typeof String(data.mid));
+
+        if (String(data.mid) == mid) {
+          console.log("true");
+          btn.classList.remove("fa-regular");
+          btn.classList.add("fa-solid");
+        } else {
+          console.log("false");
+          btn.classList.remove("fa-solid");
+          btn.classList.add("fa-regular");
+        }
+      } else {
+        console.log("no data : ", data);
+        return;
+      }
+    });
+  // }
+};
+
+// if ((user != "anonymousUser") & (user == user2)) {
+showWish();
+// }
+
 if ((user != "anonymousUser") & (user == user2)) {
   wishBtn.addEventListener("click", (e) => {
     e.preventDefault();
     const btn = e.target.querySelector("i");
 
     if (btn.classList.contains("fa-heart")) {
+      console.log("classList : ", btn.classList);
       if (btn.classList.contains("fa-regular")) {
         btn.classList.remove("fa-regular");
         btn.classList.add("fa-solid");
@@ -54,6 +89,7 @@ if ((user != "anonymousUser") & (user == user2)) {
           .then((response) => response.text())
           .then((data) => {
             console.log(data + "번 제품 찜 목록 제거");
+            showWish();
           });
       }
     }
@@ -61,36 +97,3 @@ if ((user != "anonymousUser") & (user == user2)) {
     showWish();
   });
 }
-
-const showWish = () => {
-  const btn = wishBtn.querySelector(".fa-heart");
-  console.log("찜목록 wish : ", user);
-
-  console.log("type of pno, mid : ", typeof pno, typeof mid);
-
-  // if (user != 'anonymousUser') {
-  fetch(`/wish/${pno}/${mid}`)
-    .then((response) => response.json())
-    .then((data) => {
-      if (data) {
-        console.log("data.mid: ", data.mid);
-        console.log("mid: ", mid);
-
-        if (data.mid == mid) {
-          btn.classList.remove("fa-regular");
-          btn.classList.add("fa-solid");
-        } else {
-          btn.classList.remove("fa-solid");
-          btn.classList.add("fa-regular");
-        }
-      } else {
-        console.log("no data : ", data);
-        return;
-      }
-    });
-  // }
-};
-
-// if ((user != "anonymousUser") & (user == user2)) {
-showWish();
-// }
